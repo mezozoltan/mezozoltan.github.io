d7dd382c6868b73897126d7372ab33903ff2c613
mezozoltan <120284546+mezozoltan@users.noreply.github.com>
2025-08-04 23:58:36 +0200

    Add files via upload

---
+++ games.js
@@ -0,0 +1,36 @@
+export const games = [
+  {
+    name: "Resident Evil 2",
+    logoUrl: "https://mezozoltan.github.io/Game-Images/re2logo.webp",
+    description: "A deadly virus engulfs the residents of Raccoon City in September of 1998, plunging the city into chaos as flesh eating zombies roam the streets for survivors. An unparalleled adrenaline rush, gripping storyline, and unimaginable horrors await you.",
+    backgroundUrl: "https://mezozoltan.github.io/Game-Images/re2.webp",
+    expandedBackgroundUrl: "https://mezozoltan.github.io/Game-Images/re22.webp",
+    link: "https://store.steampowered.com/app/883710/Resident_Evil_2/",
+    tags: [
+      { name: "Survival-horror", rank: 1 },
+    ]
+  },
+  {
+    name: "Resident Evil 3",
+    logoUrl: "https://mezozoltan.github.io/Game-Images/re3logo.webp",
+    description: "Jill Valentine is one of the last remaining people in Raccoon City to witness the atrocities Umbrella performed. To stop her, Umbrella unleashes their ultimate secret weapon: Nemesis!",
+    backgroundUrl: "https://mezozoltan.github.io/Game-Images/re3.webp",
+    expandedBackgroundUrl: "https://mezozoltan.github.io/Game-Images/re32.webp",
+    link: "https://store.steampowered.com/app/952060/Resident_Evil_3/",
+    tags: [
+      { name: "Survival-horror", rank: 3 },
+      { name: "Story", rank: 1 },
+    ]
+  },
+  {
+    name: "Resident Evil 7",
+    logoUrl: "https://mezozoltan.github.io/Game-Images/re7logo.webp",
+    description: "Fear and isolation seep through the walls of an abandoned southern farmhouse.",
+    backgroundUrl: "https://mezozoltan.github.io/Game-Images/re7.webp",
+    expandedBackgroundUrl: "https://mezozoltan.github.io/Game-Images/re72.webp",
+    link: "https://store.steampowered.com/app/418370/Resident_Evil_7_Biohazard/",
+    tags: [
+      { name: "Survival-horror", rank: 2 },
+    ]
+  }
+];
